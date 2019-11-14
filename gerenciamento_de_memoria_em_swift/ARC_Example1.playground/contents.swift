f69ab8321e8b176d673defe741d807c48f654fde
Txai Wieser <txaidw@gmail.com>
2019-11-14 10:28:02 -0300

    Add memory management talk

---
+++ gerenciamento_de_memoria_em_swift/ARC_Example1.playground/contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+
+
+class Pessoa { }
+
+
+var ref1: Pessoa?
+
+ref1 = Pessoa()
+
+
+var ref2: Pessoa?
+
+ref2 = ref1
+
+
+ref1 = nil
+
+ref2 = nil
